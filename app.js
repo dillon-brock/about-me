0748c27151eda84d112dfe1c0d38664acb7e367d
Dillon Brock <mdillon1297@gmail.com>
2022-06-01 16:39:44 -0700

    button shows animal section

--- app.js
+++ app.js
@@ -2,12 +2,19 @@
 
 const tosCheckBox = document.getElementById('tos-checkbox');
 const answerButton = document.getElementById('show-answer-button');
+const animalSection = document.getElementById('animal-section');
 
-tosCheckBox.addEventListener('change', () => {
-    answerButton.disabled = !tosCheckBox.checked;
-});
+
 
 // set event listeners
     // get info from user input
     // use user input to update state
     // update DOM to reflect the new state
+
+tosCheckBox.addEventListener('change', () => {
+    answerButton.disabled = !tosCheckBox.checked;
+});
+
+answerButton.addEventListener('click', () => {
+    animalSection.classList.remove('hidden');
+});
