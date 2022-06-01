2bbd0ffb181ffff36fbd9f4442fbbaa78bc1f889
Dillon Brock <mdillon1297@gmail.com>
2022-06-01 16:28:06 -0700

    TOS enables button

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 // grab DOM elements
 
+const tosCheckBox = document.getElementById('tos-checkbox');
+const answerButton = document.getElementById('show-answer-button');
+
+tosCheckBox.addEventListener('change', () => {
+    answerButton.disabled = !tosCheckBox.checked;
+});
+
 // set event listeners
     // get info from user input
     // use user input to update state
